19f62cc26a96e9751e9fba31966ff7942582e242
Vladimir <mr.tommyjacket@gmail.com>
2017-04-30 19:29:00 +0500

    Update important_parameters.py

--- important_parameters.py
+++ important_parameters.py
@@ -1,15 +1,4 @@
 import psycopg2
 
-token = ""
-db_url = ""
-
-'''db_info = {"HOST": "localhost", "NAME": "schedule_bot", "USER": "postgres", "PASSWORD": "root", "PORT": 5432}
-
-connection = psycopg2.connect(database=db_info.get("NAME"),
-                              user=db_info.get("USER"),
-                              password=db_info.get("PASSWORD"),
-                              host=db_info.get("HOST"),
-                              port=db_info.get("PORT"))
-
-cursor = connection.cursor()
-print(connection)'''
+token = <BOT TOKEN>
+db_url = <Database URL>
