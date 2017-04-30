f1446e26f6a03d4dc525db9e0ae0c55111496c61
Vladimir <mr.tommyjacket@gmail.com>
2017-04-30 19:28:20 +0500

    Update important_parameters.py

--- important_parameters.py
+++ important_parameters.py
@@ -1,7 +1,7 @@
 import psycopg2
 
-token = "318898825:AAHXNhO8LY4cA9DYH9gikhhYoPkO1RmC_do"
-db_url = "postgres://wowvqvtg:_TG_4Ty1OAkYilf96XCx-AXfxyAPDr7E@horton.elephantsql.com:5432/wowvqvtg"
+token = ""
+db_url = ""
 
 '''db_info = {"HOST": "localhost", "NAME": "schedule_bot", "USER": "postgres", "PASSWORD": "root", "PORT": 5432}
 
